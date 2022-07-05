74251eae17ae98dd72f1a98b99c9fec746ba74bc
JONYANDUNH <1309634881@qq.com>
2022-07-05 11:39:28 +0800

    change

--- src/main/java/DeginxApiJava/MainVerticle.java
+++ src/main/java/DeginxApiJava/MainVerticle.java
@@ -6,6 +6,8 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
+import java.util.HashMap;
+
 public class MainVerticle extends AbstractVerticle {
   @Override
   public void start() throws Exception {
@@ -27,41 +29,23 @@ public class MainVerticle extends AbstractVerticle {
         )
       );
   }
-  public enum ResponseCode {
-    SUCCESS("Success", 200),
-    SERVER_ERROR("Server Error", 500),
-    NOT_FOUND("Not Found", 404),
-    ERROR("Error", 400),
-    FORBIDDEN("Forbidden", 403),
-    FORM_DATA_NOT_ACCURATE("Verify that the form data is accurate", 419),
-    USER_ALREADY_EXISTS("The user already exists", 452),
-    CREATE_USER_ERROR("Failed to create user, please try again", 453),
-    LOGIN_ERROR("The user does not exist or has an incorrect password", 454);
-    // 成员变量
-    private String message;
-    private int code;
-    // 构造方法
-    private ResponseCode(String message, int code) {
-      this.message = message;
-      this.code = code;
-    }
-    // 普通方法
-    public static String GetMessage(int index) {
-      for (ResponseCode c : ResponseCode.values()) {
-        if (c.getCode() == index) {
-          return c.message;
-        }
-      }
-      return null;
-    }
-    // get set 方法
-    public int getCode() {
-      return code;
-    }
-  }
+  public static String GetMessage(int code){
+    HashMap<Integer, String> ResponseCode = new HashMap<Integer, String>();
+    ResponseCode.put(200,"Success");
+    ResponseCode.put(500,"Server Error");
+    ResponseCode.put(404,"Not Found");
+    ResponseCode.put(400,"Error");
+    ResponseCode.put(403,"Forbidden");
+    ResponseCode.put(419,"Verify that the form data is accurate");
+    ResponseCode.put(452,"The user already exists");
+    ResponseCode.put(453,"Failed to create user, please try again");
+    ResponseCode.put(454,"The user does not exist or has an incorrect password");
+    return ResponseCode.get(code);
 
+
+  };
   public static void JsonResponese(RoutingContext ctx, int code, JsonObject data) {
-    String message=ResponseCode.GetMessage(code);
+    String message=GetMessage(code);
     JsonObject json = new JsonObject().put("code",code).put("message",message).put("data",data);
     ctx.response()
       .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
@@ -69,7 +53,7 @@ public class MainVerticle extends AbstractVerticle {
 
   }
   public static void StringResponese(RoutingContext ctx,int code, String data) {
-    String message=ResponseCode.GetMessage(code);
+    String message=GetMessage(code);
     JsonObject json = new JsonObject().put("code",code).put("message",message).put("data",data);
     ctx.response()
       .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
