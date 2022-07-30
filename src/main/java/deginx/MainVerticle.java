fcd6ed429c8bf5534399ecfd5f5de75f10b65a2f
JONYANDUNH <jonyandunh@outlook.com>
2022-07-30 09:55:13 +0800

    打包成jar

--- src/main/java/deginx/MainVerticle.java
+++ src/main/java/deginx/MainVerticle.java
@@ -19,8 +19,6 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.*;
 import io.vertx.ext.web.sstore.redis.RedisSessionStore;
 import io.vertx.redis.client.Redis;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.stream.IntStream;
@@ -30,7 +28,7 @@ import java.util.stream.IntStream;
 public class MainVerticle extends AbstractVerticle {
 
     static final Vertx vertx = Vertx.vertx();
-    final static Logger logger = LoggerFactory.getLogger("loggerName");
+
     public static AuthenticationProvider provider = new UserAuth();
     public static final AuthenticationHandler basicAuthHandler = BasicAuthHandler.create(provider);
     public static long AuthExpireSeconds = 3600*24*30;
@@ -42,12 +40,7 @@ public class MainVerticle extends AbstractVerticle {
         //.put("connection_string", "mongodb://deginx-mongodb:ZlRhwajBflUGcufeICIQg7bdeGNxg5WnrMmbSx83NLRbRupCcBVjYrg0gauxT5Ion9HlACeDchvrfKQkVSlMDg==@deginx-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@deginx-mongodb@")
         .put("connection_string", "mongodb://localhost:27017")
         .put("db_name", "DeginxApi"));
-    final Handler<RoutingContext> loggingHandler = routingContext -> {
 
-        logger.info("Logging this message!");
-        routingContext.next();
-        // here you access properties of routingContext.request() and log what you want
-    };
     Redis redis = Redis.createClient(vertx, "redis://127.0.0.1:6379");
     RedisSessionStore redisSessionStore = RedisSessionStore.create(vertx, redis);
 
@@ -56,6 +49,7 @@ public class MainVerticle extends AbstractVerticle {
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> e.printStackTrace());
         // 启动 Vertx
         new MainVerticle().start();
+
     }
 
     @Override
@@ -72,7 +66,6 @@ public class MainVerticle extends AbstractVerticle {
         router.route().handler(SessionHandler.create(redisSessionStore));
         router.route("/api/*").subRouter(new ApiRoutes().create(vertx));
         router.route("/tools/*").subRouter(new ApiRoutes().create(vertx));
-        router.route().handler(loggingHandler);
         vertx.createHttpServer()
             .requestHandler(router)
             .exceptionHandler(exec->{
